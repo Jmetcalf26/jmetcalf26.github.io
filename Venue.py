4ed47b6eb67a498c0529c2d9725577205ba24691
jmetcalf26 <jackmetcalf26@gmail.com>
2025-10-22 08:28:50 -0400

    scraping broken, forbidden

--- Venue.py
+++ Venue.py
@@ -6,49 +6,17 @@ class Venue:
         self.url = url
         self.name = name
         self.headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
 }
 
     def getHTML(self):
         r = requests.get(self.url, self.headers)
-        with open('pages'+self.name+'.html', 'wb') as of:
+        with open('pages/'+self.name+'.html', 'wb') as of:
             of.write(r.content)
 
         return BeautifulSoup(r.content, 'html.parser')
         
-    def parse(self):
+    def parse(self, soup):
         pass
 
 url = 'https://www.930.com/#upcoming-shows-title'
-
-r = requests.get(url, headers=headers)
-with open('pages/930.html', 'wb') as of:
-    of.write(r.content)
-
-soup = BeautifulSoup(r.content, 'html.parser')
-
-upcoming_shows = soup.select(".list-view-item")
-
-for a in upcoming_shows:
-    dates = a.find('span', class_="dates")
-    if dates is not None:
-        print("Date:", dates.text.strip().split())
-    doors = a.find('span', class_="doors")
-    if doors is not None:
-        print("Doors:", doors.text.strip())
-    artist_info = a.find(class_="headliners")
-    supports = a.find(class_="supports")
-    if artist_info is not None:
-        print("Artist:", artist_info.text.strip())
-    else:
-        print("Artist: N/A")
-    if supports is not None:
-        print("Opener:", supports.text.strip())
-    else:
-        print("Opener: N/A")
-    ticket_price = a.find("section", class_="ticket-price")
-    sold_out = a.find(class_="sold-out")
-    if sold_out is not None:
-        print("SOLD OUT:", sold_out)
-    elif ticket_price is not None:
-        print("Ticket link:", ticket_price.a['href'])
--- scrape.py
+++ scrape.py
@@ -1,39 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+from NineThirty import NineThirty
 
-url = 'https://www.930.com/#upcoming-shows-title'
+def scrape():
+    nt = NineThirty(url='https://www.930.com/#upcoming-shows-title', name="930")
+    print(nt.name)
+    print(nt.url)
+    nt_soup = nt.getHTML()
+    nt.parse(nt_soup)
 
-headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-}
-r = requests.get(url, headers=headers)
-with open('pages/930.html', 'wb') as of:
-    of.write(r.content)
-
-soup = BeautifulSoup(r.content, 'html.parser')
-
-upcoming_shows = soup.select(".list-view-item")
-
-for a in upcoming_shows:
-    dates = a.find('span', class_="dates")
-    if dates is not None:
-        print("Date:", dates.text.strip().split())
-    doors = a.find('span', class_="doors")
-    if doors is not None:
-        print("Doors:", doors.text.strip())
-    artist_info = a.find(class_="headliners")
-    supports = a.find(class_="supports")
-    if artist_info is not None:
-        print("Artist:", artist_info.text.strip())
-    else:
-        print("Artist: N/A")
-    if supports is not None:
-        print("Opener:", supports.text.strip())
-    else:
-        print("Opener: N/A")
-    ticket_price = a.find("section", class_="ticket-price")
-    sold_out = a.find(class_="sold-out")
-    if sold_out is not None:
-        print("SOLD OUT:", sold_out)
-    elif ticket_price is not None:
-        print("Ticket link:", ticket_price.a['href'])
+#if __name__=="__main__":
+scrape()
